9854bd6bf573f0481926a681e7a55964fec07d93
wang <18522531324@163.com>
2019-03-16 13:59:24 +0800

    Initial commit

---
+++ cloud-ureaka/src/main/java/com/athome/gmall/cloudureaka/CloudUreakaApplication.java
@@ -0,0 +1,13 @@
+package com.athome.gmall.cloudureaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudUreakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudUreakaApplication.class, args);
+	}
+
+}
